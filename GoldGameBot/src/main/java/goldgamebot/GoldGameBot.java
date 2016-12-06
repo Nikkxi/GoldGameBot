d087c3232c3f7e973472b350c612e3e74f5c0eb8
SupCMDr <eriknpeters@gmail.com>
2016-12-05 22:44:51 -0500

    Bug Fixes
    Fixed several issues:
    -Players not being added to the round
    -Error handling not working properly for several commands
    -Rolls failing to process correctly
    -Results failing to process correctly
    -Fixed issue where the results reply was being posted before the reply
    containing the last person's roll (added a thread wait delay)
    -Several other unlisted bugs...

--- GoldGameBot/src/main/java/goldgamebot/GoldGameBot.java
+++ GoldGameBot/src/main/java/goldgamebot/GoldGameBot.java
@@ -27,7 +27,7 @@ public class GoldGameBot {
 	private static List<Player> playerList;
 	private static int numPlayersRolled = 0;
 	private static String gameOwner;
-	private static String[] whoCanReset = null;
+	private static List<String> whoCanReset;
 
 
 	public static void main(String[] args) {
@@ -55,16 +55,22 @@ public class GoldGameBot {
 		final String boundChannel = prop.getProperty("bindToChannel");
 		final String commandPrefix = prop.getProperty("CommandPrefix");
 		final String allowedToReset = prop.getProperty("canReset");
-		
-		
+
+		System.out.println();
+
 		if(allowedToReset.contains(",")){
-			whoCanReset = allowedToReset.split(",");
-		}else{
-			whoCanReset[0] = allowedToReset;
+			for(int a = 0; a < allowedToReset.split(",").length; a++){
+				whoCanReset.add(allowedToReset.split(",")[a]);
+			}
+		}else if(!allowedToReset.isEmpty()){
+			whoCanReset.add(allowedToReset);
 		}
 
 		DiscordAPI api = Javacord.getApi(token, true);
 		api.setGame("GoldGameBot");
+
+		// INITIALIZE PLAYER LIST
+		playerList = new ArrayList<Player>();
 
 		// CONNECT BOT
 		api.connect(new FutureCallback<DiscordAPI>(){
@@ -77,6 +83,7 @@ public class GoldGameBot {
 				api.registerListener(new MessageCreateListener(){
 					public void onMessageCreate(DiscordAPI api, Message message){
 
+						System.out.println("1 - received message");
 						boolean isBound = false;
 						boolean fromBoundChannel = false;
 						String playerId = message.getAuthor().getId();
@@ -84,34 +91,43 @@ public class GoldGameBot {
 						if(!boundChannel.isEmpty())
 							isBound = true;
 
+						System.out.println("2 - isBoundChannel - " + isBound);
+
 						if(message.getReceiver().getId().equalsIgnoreCase(boundChannel) || !isBound)
 							fromBoundChannel = true;
+
+						System.out.println("3 - fromBoundChannel = " + fromBoundChannel);
 
 
 						// START GOLD GAME
 						if(fromBoundChannel && message.getContent().toUpperCase().startsWith(commandPrefix + "GOLDGAME")){
 
-							String action = message.getContent().toUpperCase().substring(10);
-							System.out.println("Action = " + action);
-
+							String action = null;
 							int command = 0;
 
 							// DETERMINE WHICH COMMAND WAS USED
-							if(action.toUpperCase().startsWith("START")){
-								command = 1;
-							}else if(action.toUpperCase().startsWith("BET")){
-								command = 2;
-							}else if(action.toUpperCase().startsWith("JOIN")){
-								command = 3;
-							}else if(action.toUpperCase().startsWith("CLOSE")){
-								command = 4;
-							}else if(action.toUpperCase().startsWith("ROLL")){
-								command = 5;
-							}else if(action.toUpperCase().startsWith("RESET")){
-								command = 6;
-							}else if(action.toUpperCase().startsWith("HELP")){
-								command = 7;
+							try{
+								action = message.getContent().toUpperCase().substring(10);
+
+								if(action.toUpperCase().startsWith("START")){
+									command = 1;
+								}else if(action.toUpperCase().startsWith("BET")){
+									command = 2;
+								}else if(action.toUpperCase().startsWith("JOIN")){
+									command = 3;
+								}else if(action.toUpperCase().startsWith("CLOSE")){
+									command = 4;
+								}else if(action.toUpperCase().startsWith("ROLL")){
+									command = 5;
+								}else if(action.toUpperCase().startsWith("RESET")){
+									command = 6;
+								}else if(action.toUpperCase().startsWith("HELP")){
+									command = 7;
+								}
+							}catch(Exception e){
+								//message.reply("No command was recognized.  Please try again.");
 							}
+							System.out.println("Action = " + action);
 
 							// EXECUTE THE COMMAND
 							switch(command){
@@ -123,11 +139,11 @@ public class GoldGameBot {
 									message.reply("Starting a new Gold Game.  Please specify a bet amount using '"
 											+ commandPrefix + "GOLDGAME BET <AMOUNT>'" );
 								}else{
-									message.reply("@" + playerId + " A Gold Game is already running!");
+									message.reply("<@" + playerId + "> A Gold Game is already running!");
 								}
 								break;
 							case 2: // BET
-								if(isGameRunning && message.getAuthor().getId().equalsIgnoreCase(gameOwner)){
+								if(isGameRunning && !isBetSet && message.getAuthor().getId().equalsIgnoreCase(gameOwner)){
 									try{
 										betAmount = Integer.parseInt(message.getContent().substring(14));
 									}catch(Exception e){
@@ -138,26 +154,47 @@ public class GoldGameBot {
 									message.reply("Bet amount for the current game is now set to " + betAmount + " gold.\n"
 											+ "To join the current Gold Game, please use the command '" + commandPrefix + 
 											"GOLDGAME JOIN'" );
+								}else if(isGameRunning && isBetSet && message.getAuthor().getId().equalsIgnoreCase(gameOwner)){
+									message.reply("<@" + playerId + "> The bet has already been set! Current bet is " + betAmount);
 								}else if(isGameRunning){
-									message.reply("@" + playerId + " You do not have permission to set the bet amount for this round.");
+									message.reply("<@" + playerId + "> You do not have permission to set the bet amount for this round.");
 								}else{
-									message.reply("@" + playerId + " No Gold Game is currently in-progress.");
+									message.reply("<@" + playerId + "> No Gold Game is currently in-progress.");
 								}
 								break;
 							case 3: // JOIN
 								if(isBetSet && isGameRunning && !isRegistrationClosed){
-									Player player = new Player(playerId);
-									playerList.add(player);
-									message.reply("@" + playerId + " has joined this round!  Players joined: " + playerList.size());
+									if(playerList.isEmpty()){
+										Player player = new Player(playerId);
+										playerList.add(player);
+										message.reply("<@" + playerId + "> has joined this round!  Players joined: " + playerList.size());
+									}else{
+										boolean playerExists = false;
+										for(Player playerX:playerList){
+											if(playerX.getPlayerID().equalsIgnoreCase(playerId)){
+												playerExists = true;
+											}
+										}
+										if(!playerExists){
+											Player player = new Player(playerId);
+											playerList.add(player);
+											message.reply("<@" + playerId + "> has joined this round!  Players joined: " + playerList.size());
+										}else{
+											message.reply("<@" + playerId + "> you have already joined this round.");
+										}
+									}									
 								}else{
+
 									if(isGameRunning && isBetSet && isRegistrationClosed)
-										message.reply("@" + playerId + " Registration for this round has already closed.");
+										message.reply("<@" + playerId + "> Registration for this round has already closed.");
 									else if(!isBetSet && isGameRunning)
 										message.reply("Bet amount has not been set yet.  Please wait.");
 									else if(!isGameRunning)
-										message.reply("@" + playerId + " There is no Gold Game currently running.");
+										message.reply("<@" + playerId + "> There is no Gold Game currently running.");
 									else
 										message.reply("Something went wrong... (ERROR 01)");
+
+									//message.reply("<@" + playerId + "> Something happened and you were unable to be added to the current round.");
 								}
 								break;
 							case 4: // CLOSE
@@ -167,15 +204,15 @@ public class GoldGameBot {
 									message.reply("Registration for this round is now closed!  Roll using '" + commandPrefix + "GOLDGAME ROLL'");
 								}else{
 									if(!message.getAuthor().getId().equalsIgnoreCase(gameOwner))
-										message.reply("@" + playerId + " You do not have permissions to close registration for the current round.");
+										message.reply("<@" + playerId + "> You do not have permissions to close registration for the current round.");
 									else if(isBetSet && isGameRunning && (playerList.size() < 2))
-										message.reply("@" + playerId + " Not enough players have joined this round yet!");
+										message.reply("<@" + playerId + "> Not enough players have joined this round yet!");
 									else if(isGameRunning)
-										message.reply("@" + playerId + " The bet amount has not been set yet!");
+										message.reply("<@" + playerId + "> The bet amount has not been set yet!");
 									else if(!isGameRunning)
-										message.reply("@" + playerId + " There is no Gold Game currently running.");
+										message.reply("<@" + playerId + "> There is no Gold Game currently running.");
 									else
-										message.reply("@" + playerId + " Something went wrong... (ERROR 02).");
+										message.reply("<@" + playerId + "> Something went wrong... (ERROR 02).");
 								}
 								break;
 							case 5: // ROLL
@@ -192,29 +229,32 @@ public class GoldGameBot {
 										}
 									}
 
-									message.reply("@" + playerId + " has rolled " + result);
+									message.reply("<@" + playerId + "> has rolled " + result);
 
 									Player largest = null;
 									Player smallest = null;
 									if(numPlayersRolled == playerList.size()){
 
-										message.reply("\n\n-------------------------------------------\n\n");
-
+										
+										message.reply("\n\n");
+										
 										largest = playerList.get(0);
 										smallest = playerList.get(0);
 
-										for(int i = 1; i < playerList.size()-1; i++){
+										for(int i = 0; i < playerList.size(); i++){
 
 											// IN CASE OF TIE FOR LARGEST
 											if(playerList.get(i).getResult() == largest.getResult()){
 
 												int rand = (int)Math.random();  // Result is either 0 or 1
+												System.out.println("Tie for the largest rand roll = " + rand);
 
 												if(rand == 1){
 													largest = playerList.get(i);
+													System.out.println("The player who won the tie is " + largest.getPlayerID());
 												}
 
-											// NEXT PLAYER HAS HIGHER RESULT	
+												// NEXT PLAYER HAS HIGHER RESULT	
 											}else if(playerList.get(i).getResult() > largest.getResult()){
 												largest = playerList.get(i);
 											}
@@ -224,32 +264,59 @@ public class GoldGameBot {
 											// IN CASE OF TIE FOR SMALLEST
 											if(playerList.get(i).getResult() == smallest.getResult()){
 												int rand = (int)Math.random();  // Result is either 0 or 1
+												System.out.println("Tie for the lowest rand roll = " + rand);
 
 												if(rand == 1){
 													smallest = playerList.get(i);
+													System.out.println("the player who won the tie is " + smallest.getPlayerID());
 												}
 
-											// NEXT PLAYER HAS LOWER RESULT
+												// NEXT PLAYER HAS LOWER RESULT
 											}else if(playerList.get(i).getResult() < smallest.getResult()){
 												smallest = playerList.get(i);
 											}	
 										}
+
+										try{
+											
+											synchronized(this){
+												this.wait(1000);	
+											}
+											
+											int owedGold = largest.getResult() - smallest.getResult();
+											
+											// BUILD THE RESULTS MESSAGE
+											String results = new String("");
+											
+											results = results.concat("\n\n-------------------------------------------\n\n" + 
+													  "Everyone has rolled!  Here are the results! \n\n");
+
+											for(int index = 0; index < playerList.size(); index++){
+												results = results.concat("<@" + playerList.get(index).getPlayerID() + "> rolled " + playerList.get(index).getResult() + "\n");
+											}
+											
+											results = results.concat("\n<@" + smallest.getPlayerID() + ">  owes " + owedGold + " gold to " + "<@" + largest.getPlayerID() + ">  !!");
+											
+											// POST THE RESULTS MESSAGE
+											message.reply(results);
+										}catch(NullPointerException e){
+											message.reply("Something went wrong. (ERROR 03)");
+											e.printStackTrace();
+										} catch (InterruptedException e) {
+											e.printStackTrace();
+										}
+										resetGame();
 									}
 
-									try{
-										message.reply("@" + smallest.getPlayerID() + "  owes " + " gold to " + "@" + largest.getPlayerID() + "  !!");
-									}catch(NullPointerException e){
-										message.reply("Seomething went wrong. (ERROR 03)");
-									}
-									resetGame();
+
 
 								}else{
 									if(isGameRunning && isBetSet)
-										message.reply("@" + playerId + " Registration has not closed for the current round yet.");
+										message.reply("<@" + playerId + "> Registration has not closed for the current round yet.");
 									else if(isGameRunning && !isBetSet)
-										message.reply("@" + playerId + " Registration is not open yet. The bet amount still needs to be set.");
+										message.reply("<@" + playerId + "> Registration is not open yet. The bet amount still needs to be set.");
 									else if(!isGameRunning)
-										message.reply("@" + playerId + " There is no Gold Game running!");
+										message.reply("<@" + playerId + "> There is no Gold Game running!");
 									else
 										message.reply("Something went wrong. (ERROR 04)");
 								}
@@ -258,14 +325,17 @@ public class GoldGameBot {
 								break;
 							case 6:  // RESET
 								String author = message.getAuthor().getId();
-								for(int k = 0; k < whoCanReset.length; k++){
-									if(author.equalsIgnoreCase(whoCanReset[k]))
-										resetGame();
+								if(whoCanReset.size() > 0){
+									for(int k = 0; k < whoCanReset.size(); k++){
+										if(author.equalsIgnoreCase(whoCanReset.get(k)))
+											resetGame();
+									}
 								}
-								
-								if(message.getAuthor().getId().equalsIgnoreCase(gameOwner))
+
+								if(author.equalsIgnoreCase(gameOwner))
 									resetGame();
-								
+
+								message.reply("The Gold Game has ended!");
 								break;
 							case 7:  // HELP
 								message.reply("Welcome to the GoldGameBot HELP.  Here are the available commands: \n" +
@@ -280,7 +350,7 @@ public class GoldGameBot {
 										);
 								break;
 							default: 
-								message.reply("Something's not right... That command was not recognized.");
+								message.reply("Something's not quite right... That command was not recognized.");
 
 								break;
 
